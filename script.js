3c045575141455a1d5bbce7997018d5da528e2e5
pankajkumar199 <pk228624@gmail.com>
2025-11-11 08:38:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+document.getElementById("submit").addEventListener("click", function () {
+    
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let isbn = document.getElementById("isbn").value;
+
+    if (title === "" || author === "" || isbn === "") {
+        alert("Please fill all fields");
+        return;
+    }
+
+    let table = document.getElementById("book-list");
+
+    let row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${title}</td>
+        <td>${author}</td>
+        <td>${isbn}</td>
+        <td><button class="delete">X</button></td>
+    `;
+
+    table.appendChild(row);
+
+    // Clear fields
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+});
+
+// Delete Row
+document.getElementById("book-list").addEventListener("click", function (e) {
+    if (e.target.classList.contains("delete")) {
+        e.target.parentElement.parentElement.remove();
+    }
+});
